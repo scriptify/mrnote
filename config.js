b7180344378d2e37393945468f894d69fb53442a
Maximilian Torggler <maximilian.torggler@tuta.io>
2016-06-27 21:00:28 +0200

    Now hosting on a root server

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   PUBLIC_PATH: path.join(__dirname, '/client/build'),
   APP_PATH: path.join(__dirname, '/client/app'),
   IMG_PATH: path.join(__dirname, '/client/app/img'),
-  DB_URL: `${(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017/')}noteboard`,
-  API_PATH: '/api/', 
-  SERVER_PORT: process.env.OPENSHIFT_NODEJS_PORT || 3000,
-  SERVER_IP: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  CORS: true
+  DB_URL: 'mongodb://localhost:27017/noteboard',
+  API_PATH: '/api/',
+  SERVER_PORT: 443,
+  SERVER_IP: '127.0.0.1',
+  CORS: false
 };
