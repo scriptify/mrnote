27cf4de1e7d6a9109f34c3a3ce485352663b544b
Maximilian Torggler <maximilian.torggler@tuta.io>
2016-06-30 19:51:29 +0200

    Added feature: Now, when in development mode (node run dev) both the API CRUD server and the webpack-dev-server are automatically started.

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
+const TARGET = process.env.npm_lifecycle_event;
+
 module.exports = {
   PUBLIC_PATH: path.join(__dirname, '/client/build'),
   APP_PATH: path.join(__dirname, '/client/app'),
   IMG_PATH: path.join(__dirname, '/client/app/img'),
   DB_URL: 'mongodb://localhost:27017/noteboard',
   API_PATH: '/api/',
-  SERVER_PORT: 8080,
+  SERVER_PORT: (TARGET === 'dev') ? 3000 : 8080,
   SERVER_IP: '127.0.0.1',
-  CORS: true,
-  IS_API_SERVER_EXTERNAL: 
-    (process.env.NODE_ENV !== 'production')
+  CORS: (TARGET === 'dev'),
+  IS_API_SERVER_EXTERNAL: (TARGET === 'dev')
 };
