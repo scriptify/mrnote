3cc775171d95a54bcfd2f4a79b0cb1858731dd80
Maximilian Torggler <maximilian.torggler@tuta.io>
2016-07-03 12:15:09 +0200

    Fixed paths in config file.

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ const path = require('path');
 const TARGET = process.env.npm_lifecycle_event;
 
 module.exports = {
-  PUBLIC_PATH: path.join(__dirname, '/client/build'),
-  APP_PATH: path.join(__dirname, '/client/app'),
-  IMG_PATH: path.join(__dirname, '/client/app/img'),
+  PUBLIC_PATH: path.join(__dirname, '../client/build'),
+  APP_PATH: path.join(__dirname, '../client/app'),
+  IMG_PATH: path.join(__dirname, '../client/app/img'),
   CONFIG_PATH: path.join(__dirname, '.'),
   DB_URL: 'mongodb://localhost:27017/noteboard',
   API_PATH: '/api/',
